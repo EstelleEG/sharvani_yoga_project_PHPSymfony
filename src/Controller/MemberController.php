1e09734a2d08d0f17dc3ff9f8e82ff73f1cb31cb
Estelle <estellegregoire0689@gmail.com>
2022-06-13 17:15:16 +0200

    updates

--- src/Controller/MemberController.php
+++ src/Controller/MemberController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/member', name: '')]
+#[Route('/member', name: 'app_member')]
 
 class MemberController extends AbstractController
 {
